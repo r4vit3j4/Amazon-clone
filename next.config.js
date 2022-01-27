b9b6340c6dc308d745ff24c01964b08d8096649c
Ravi Teja <75667275+r4vit3j4@users.noreply.github.com>
2022-01-27 22:05:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,4 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
-  env: {
-    NEXT_PUBLIC_GOOGLE_ID=process.env.NEXT_PUBLIC_GOOGLE_ID,
-    NEXT_PUBLIC_GOOGLE_SECRET=process.env.NEXT_PUBLIC_GOOGLE_SECRET,
-    NEXT_PUBLIC_NEXTAUTH_URL=process.env.NEXT_PUBLIC_NEXTAUTH_URL,
-    NEXT_PUBLIC_HOST=process.env.NEXT_PUBLIC_HOST,
-  }
 };
