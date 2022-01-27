c9b8d92f1a3c44f8cc40f7afb6340c609060e214
Ravi Teja <raviteja281003@gmail.com>
2022-01-27 21:59:25 +0530

    Revert "Api key update"
    This reverts commit 74200934d6ffa2025f13abe57c53cc0324b07c88.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export default nextAuth({
   providers: [
     GoogleProvider({
-      clientId:
-        332421856982 -
-        tjji1k6i7k1chkfbof0cpcps5dkeo4ap.apps.googleusercontent.com,
-      clientSecret: GOCSPX - uDC02cYLzrXOw0qmGnhz7_OM59jH,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
     }),
   ],
 });
